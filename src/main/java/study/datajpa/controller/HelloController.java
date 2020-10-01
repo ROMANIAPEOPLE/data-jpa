4c1b6211c99eee2fad4fe1268432a9cd40ed404d
ROMANIAPEOPLE <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2020-10-01 17:56:09 +0900

    페이징과 정렬 - 쉽게하기

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -1,13 +1,40 @@
 package study.datajpa.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final MemberRepository memberRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
+
+
+    @GetMapping("/members")
+    public Page<Member> List(@PageableDefault(size =5, sort="username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
+    @PostConstruct
+    public void init() {
+        for(int i=0; i<100; i++){
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+
+
 }
